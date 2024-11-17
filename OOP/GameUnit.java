8bd1f0c74c30783e2c045eed8a8aebd891b57dd6
AntiFreezer <80196320+AntiFreezer@users.noreply.github.com>
2024-11-17 14:58:59 +0300

    Update GameUnit.java

--- OOP/GameUnit.java
+++ OOP/GameUnit.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-class GameUnit{
+class GameUnit implements Funcs{
     private String name = "";
     private int health = 100;
     private int strength = 20;
@@ -7,12 +7,14 @@ class GameUnit{
     private int HEIGHT = 170;
     private TypesGU Type = TypesGU.DEFAULT_UNIT;
     private String Phrase = "HI";
-    private int orig_health = 100;
+    int shield = 0;
     public GameUnit(int health, int strength, TypesGU Type){
         this.health = health;
         this.strength = strength;
         this.Type = Type;
-        this.orig_health = health;
+        if (this.Type == TypesGU.TANK){
+            this.shield = 200;
+        }
     }
     public int getHealth(){
         return this.health;
@@ -28,41 +30,47 @@ class GameUnit{
     }
     public void attack(GameUnit enemy){
         // But what to do if health is < 0 ???
-        enemy.setHealth(damage(enemy));
+        int damg = damage(enemy);
+        if(enemy.shield > 0){
+            enemy.shield -= damg;
+        }
+        else {
+            enemy.setHealth(enemy.health - damg);
+        }
+        System.out.println(damg);
     }
-    public int  damage(GameUnit enemy){
+    public int damage(GameUnit enemy){
         Random random = new Random();
         double rand = random.nextDouble();
         int res = 0;
         double plus_damage = 0;
         if(this.Type == TypesGU.TANK)
         {
-            res = (rand < 0.7) ? 1 : 0;
+            res = (rand < 0.03) ? 1 : 0;
             WEIGHT = 150;
             HEIGHT = 200;
-            if (health / orig_health < 0.3) {
-                plus_damage = (double)enemy.getHealth() * 0.3;
-            }
+            plus_damage = res*((double)WEIGHT/(double)HEIGHT) * 10;
+            res = 1;
+            
         }
         else if(this.Type == TypesGU.HEALER)
         {
-            res = (rand < 0.4) ? 1 : 0;
-            WEIGHT = 90;
-            HEIGHT = 190;
+            res = (rand < 0.05) ? 2 : 1;
         }
         else if(this.Type == TypesGU.FlANG)
         {
-            res = (rand < 0.95) ? 1 : 0;
-            WEIGHT = 60;
-            HEIGHT = 170;
+            res = (rand < 0.15) ? 2 : 1;
         }
         else if(this.Type == TypesGU.DAMAGE_DEALER)
         {
-            res = (rand < 0.85) ? 1 : 0;
-            WEIGHT = 80;
-            HEIGHT = 185;
+            res = (rand < 0.1) ? 3 : 1;
         }
-        return (this.strength - res * (HEIGHT/WEIGHT) - (int)plus_damage);
+        return (this.strength * res + (int)plus_damage);
+    }
+    @Override
+    public void Heal(){
+        if (this.Type == TypesGU.HEALER){
+            this.health += 10;
+        }
     }
 }
-
